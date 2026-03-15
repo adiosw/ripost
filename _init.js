12f67cec2567c26e61f7d7504897fc8b208b08d1
Adrian <adiosw97@gmail.com>
2026-03-15 19:58:01 +0100

    upload: _init.js

--- _init.js
+++ _init.js
@@ -5,10 +5,6 @@
     const c = await r.json();
     window.SUPABASE_URL  = c.su;
     window.SUPABASE_ANON = c.sa;
-    window.STRIPE_PK     = c.sp;
-    window.PRICE_29      = c.p1;
-    window.PRICE_49      = c.p2;
-    window.PRICE_99      = c.p3;
   } catch(e) {
     console.warn('Config not loaded:', e);
   }
